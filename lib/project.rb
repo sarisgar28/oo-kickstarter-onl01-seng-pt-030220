f1ffc7f926344e5f8d838d92fc885ccc2f3d6419
Sara Garcia <sarisgar28@gmail.com>
2020-04-12 14:04:18 -0400

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,24 @@
+class Project 
+attr_accessor :backers 
+attr_reader :title 
+
+
+def initialize(title)
+    @title = title 
+    @backers = [ ]
+end 
+
+def add_backer(backer)
+  @backers << backer 
+  backer.backed_projects << self 
+end 
+
+def title=(title)
+    @title = title 
+end 
+
+
+
+
+
+end 
